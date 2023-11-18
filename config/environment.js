646ade09d96ce8a451d4d168b9c88548897d0799
Karan Kumar <105391077+KaranKumar-97@users.noreply.github.com>
2023-11-18 15:41:32 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -39,24 +39,23 @@ const development={
 const production={
     name:'production',
     asset_path:'./assets',
-    // asset_path:process.env.asset_path,
-    session_cookie_key: process.env.session_cookie_key,
-    db:process.env.db,
+    session_cookie_key: '29DD93AED29EAA4413D48585AA25B',
+    db:'BlissIndia',
     smtp:{
         service:'gmail',
         host:'smtp.gmail.com',
         port: 587,
         secure:false,
         auth:{
-            user:process.env.user,
-            pass:process.env.pass
+            user:'blissofficialindia',
+            pass:'ikfsjkoxlwtatozc'
         }
     },
-    google_client_id: process.env.google_client_id,
-    google_client_secret: process.env.google_client_secret,
-    google_call_back_url: process.env.google_call_back_url,
+    google_client_id: "66736520931-dcd643omimgjuof7kf7tc9j1iq8nro4s.apps.googleusercontent.com",
+    google_client_secret: "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",
+    google_call_back_url: "http://localhost:8000/users/auth/google/callback",
     morgan:{
-        mode:'combined',
+        mode:'dev',
         options:{stream:accessLogStream}
     }
     
